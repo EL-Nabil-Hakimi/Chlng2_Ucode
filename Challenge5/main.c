abeebf60cb2afa007c2cc33fe571398de17f9590
EL-Nabil-Hakimi <nabilelhakimi2023@gmail.com>
2023-09-14 12:13:17 +0100

    Challenge5

--- Challenge5/main.c
+++ Challenge5/main.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    float moi , heur , munite ;
-    printf("convertir l'année ! \n");
-    printf("Taper Le Nombre De Moi ? \n");
-    scanf("%.f" , &moi);
-    printf("Taper Le Nombre Des heure ? \n");
-    scanf("%.f" , &heur);
-    printf("Taper Le Nombre De munites ? \n");
-    scanf("%.f" , &munite);
-    ²1
+int main() {
+    int annee, jours, mois, heures, minutes;
+    printf("Choiseze une Annee = ");
+    scanf("%d" , &annee);
+
+    if ((annee % 4 == 0 && annee % 100 != 0) || (annee % 400 == 0)) {
+
+        jours = 366;
+    mois = jours / 30 ;
+    heures = jours * 24 ;
+    minutes = heures * 60 ;
+    printf("%d Jours , %d Mois , %d Heurs , %d Minutes " , jours , mois , heures , minutes  );
+
+
+        }
+    else{
+    jours = 365;
+    mois = jours / 30 ;
+    heures = jours * 24 ;
+    minutes = heures * 60 ;
+    printf("%d Jours , %d Mois , %d Heurs , %d Minutes " , jours , mois , heures , minutes  );
+
+    }
+
+
+
+    return 0;
+}
