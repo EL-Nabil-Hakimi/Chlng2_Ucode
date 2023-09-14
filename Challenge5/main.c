e5933e68adf6ebb22a2f1ebb826636021bc93147
EL-Nabil-Hakimi <nabilelhakimi2023@gmail.com>
2023-09-14 12:07:09 +0100

    Challenge5

---
+++ Challenge5/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float moi , heur , munite ;
+    printf("convertir l'année ! \n");
+    printf("Taper Le Nombre De Moi ? \n");
+    scanf("%.f" , &moi);
+    printf("Taper Le Nombre Des heure ? \n");
+    scanf("%.f" , &heur);
+    printf("Taper Le Nombre De munites ? \n");
+    scanf("%.f" , &munite);
+    ²1
