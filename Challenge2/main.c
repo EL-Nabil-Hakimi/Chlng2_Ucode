de47d0dcfeceddd4f327af92446ded37631cf0c8
EL-Nabil-Hakimi <nabilelhakimi2023@gmail.com>
2023-09-14 10:56:35 +0100

    Challenge2

---
+++ Challenge2/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char x =' ';
+
+    printf("Tapez Une Char pour virifier??\n");
+    scanf("%c" , &x);
+    switch (x){
+    case 'A' :
+        printf("une voyelle");
+        break;
+    case 'E' :
+        printf("une voyelle");
+        break;
+
+    case 'Y' :
+        printf("une voyelle");
+        break;
+    case 'I' :
+        printf("une voyelle");
+        break;
+    case 'O' :
+        printf("une voyelle");
+        break;
+    case 'U' :
+        printf("une voyelle");
+        break;
+
+        case 'a' :
+        printf("une voyelle");
+        break;
+    case 'e' :
+        printf("une voyelle");
+        break;
+
+    case 'y' :
+        printf("une voyelle");
+        break;
+    case 'i' :
+        printf("une voyelle");
+        break;
+    case 'o' :
+        printf("une voyelle");
+        break;
+    case 'u' :
+        printf("une voyelle");
+        break;
+    default :
+        printf("non C'est Pas une voyelle");
+
+    }
+
+    return 0;
+}
